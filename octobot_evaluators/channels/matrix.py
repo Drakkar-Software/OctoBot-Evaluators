15554fcad50362ba002a2e9289b6eed6d2ed9a92
Herklos <herklos.dev@protonmail.com>
2019-10-18 00:18:25 +0200

    [Version] v1.2.5

--- octobot_evaluators/channels/matrix.py
+++ octobot_evaluators/channels/matrix.py
@@ -18,12 +18,10 @@ from asyncio import CancelledError
 
 from octobot_channels import CHANNEL_WILDCARD
 from octobot_channels.channels.channel import Channel
-from octobot_channels.channels.channel_instances import ChannelInstances
 from octobot_channels.consumer import Consumer
 from octobot_channels.producer import Producer
 from octobot_commons.logging.logging_util import get_logger
 from octobot_evaluators.constants import EVALUATOR_EVAL_DEFAULT_TYPE
-
 from octobot_evaluators.data import EvaluatorMatrix
 
 MATRIX_CHANNELS = "MatrixChannels"
