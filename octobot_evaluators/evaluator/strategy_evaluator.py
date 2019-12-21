5237d3d54958a8127f14d3fca8c6646e3c9ceaab
Herklos <herklos.dev@protonmail.com>
2019-12-21 15:24:51 +0100

    Improved strategy evaluator typing

--- octobot_evaluators/evaluator/strategy_evaluator.py
+++ octobot_evaluators/evaluator/strategy_evaluator.py
@@ -51,28 +51,28 @@ class StrategyEvaluator(AbstractEvaluator):
         pass
 
     @classmethod
-    def get_required_time_frames(cls, config):
+    def get_required_time_frames(cls, config: dict):
         if CONFIG_FORCED_TIME_FRAME in config:
             return TimeFrameManager.parse_time_frames(config[CONFIG_FORCED_TIME_FRAME])
-        strategy_config = cls.get_specific_config()
+        strategy_config: dict = cls.get_specific_config()
         if STRATEGIES_REQUIRED_TIME_FRAME in strategy_config:
             return TimeFrameManager.parse_time_frames(strategy_config[STRATEGIES_REQUIRED_TIME_FRAME])
         else:
             raise Exception(f"'{STRATEGIES_REQUIRED_TIME_FRAME}' is missing in {cls.get_config_file_name()}")
 
     @classmethod
-    def get_required_evaluators(cls, config, strategy_config=None):
+    def get_required_evaluators(cls, config: dict, strategy_config: dict = None):
         if CONFIG_FORCED_EVALUATOR in config:
             return config[CONFIG_FORCED_EVALUATOR]
-        strategy_config = strategy_config or cls.get_specific_config()
+        strategy_config: dict = strategy_config or cls.get_specific_config()
         if STRATEGIES_REQUIRED_EVALUATORS in strategy_config:
             return strategy_config[STRATEGIES_REQUIRED_EVALUATORS]
         else:
             raise Exception(f"'{STRATEGIES_REQUIRED_EVALUATORS}' is missing in {cls.get_config_file_name()}")
 
     @classmethod
-    def get_default_evaluators(cls, config):
-        strategy_config = cls.get_specific_config()
+    def get_default_evaluators(cls, config: dict):
+        strategy_config: dict = cls.get_specific_config()
         if TENTACLE_DEFAULT_CONFIG in strategy_config:
             return strategy_config[TENTACLE_DEFAULT_CONFIG]
         else:
