5054b93ae1a673f4d90d997640932fb959ccd45c
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2023-05-02 17:54:00 +0200

    [Cython] remove

--- setup.py
+++ setup.py
@@ -14,50 +14,12 @@
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
 # from distutils.extension import Extension
-import os
-
-from setuptools import dist
-
-dist.Distribution().fetch_build_eggs(['Cython>=0.29.32', 'numpy==1.24.0'])
-
-import numpy as np
-
-try:
-    from Cython.Distutils import build_ext
-    from Cython.Build import cythonize
-except ImportError:
-    # create closure for deferred import
-    def cythonize(*args, **kwargs):
-        from Cython.Build import cythonize
-        return cythonize(*args, **kwargs)
-
-
-    def build_ext(*args, **kwargs):
-        from Cython.Distutils import build_ext
-        return build_ext(*args, **kwargs)
-
 from setuptools import find_packages
-from setuptools import setup, Extension
+from setuptools import setup
 
 from octobot_evaluators import VERSION, PROJECT_NAME
 
 PACKAGES = find_packages(exclude=["tests"])
-
-packages_list: list = [
-    "octobot_evaluators.octobot_channel_consumer",
-    "octobot_evaluators.evaluators.evaluator_factory",
-    "octobot_evaluators.evaluators.channel.evaluators",
-    "octobot_evaluators.evaluators.channel.evaluator_channel",
-    "octobot_evaluators.util.evaluation_util",
-    "octobot_evaluators.matrix.matrix",
-    "octobot_evaluators.matrix.matrices",
-    "octobot_evaluators.matrix.matrix_manager",
-    "octobot_evaluators.matrix.channel.matrix",
-]
-
-ext_modules: list = [
-    Extension(package, [f"{package.replace('.', '/')}.py"])
-    for package in packages_list]
 
 # long description from README file
 with open('README.md', encoding='utf-8') as f:
@@ -65,7 +27,6 @@ with open('README.md', encoding='utf-8') as f:
 
 REQUIRED = open('requirements.txt').readlines()
 REQUIRES_PYTHON = '>=3.8'
-CYTHON_DEBUG = False if not os.getenv('CYTHON_DEBUG') else os.getenv('CYTHON_DEBUG')
 
 setup(
     name=PROJECT_NAME,
@@ -78,15 +39,12 @@ setup(
     packages=PACKAGES,
     include_package_data=True,
     long_description=DESCRIPTION,
-    include_dirs=[np.get_include()],
-    cmdclass={'build_ext': build_ext},
     tests_require=["pytest"],
     test_suite="tests",
     zip_safe=False,
     data_files=[],
-    setup_requires=REQUIRED if not CYTHON_DEBUG else [],
+    setup_requires=REQUIRED,
     install_requires=REQUIRED,
-    ext_modules=cythonize(ext_modules, gdb_debug=CYTHON_DEBUG),
     python_requires=REQUIRES_PYTHON,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
